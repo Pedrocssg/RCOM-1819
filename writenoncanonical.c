bc499351dfa7bea316a1e5471c754154f84ef7d0
Duarte Oliveira <duartefmoliveira8@gmail.com>
2018-09-28 12:36:35 +0100

    Add files via upload

--- writenoncanonical.c
+++ writenoncanonical.c
@@ -1,13 +1,12 @@
-/*Non-Canonical Input Processing*/
-
+#include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <termios.h>
-#include <stdio.h>
 #include <stdlib.h>
 
-#define BAUDRATE B38400
+
+#define BAUDRATE B9600
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FALSE 0
@@ -15,93 +14,74 @@
 
 volatile int STOP=FALSE;
 
-int main(int argc, char** argv)
-{
-    int fd,c, res;
-    struct termios oldtio,newtio;
-    char buf[255];
-    int i, sum = 0, speed = 0;
+int main(int argc, char const *argv[]) {
+  int fd,c, res;
+  struct termios oldtio,newtio;
+  char buf[255];
+  int i, sum = 0, speed = 0;
 
-    if ( (argc < 2) ||
-  	     ((strcmp("/dev/ttyS0", argv[1])!=0) &&
-  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
-    }
+  if ( (argc < 2) ||
+       ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+        (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+    exit(1);
+  }
 
+/*
+  Open serial port device for reading and writing and not as controlling tty
+  because we don't want to get killed if linenoise sends CTRL-C.
+*/
 
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
+  fd = open(argv[1], O_RDWR | O_NOCTTY );
+  if (fd <0) {perror(argv[1]); exit(-1); }
 
+  if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+    perror("tcgetattr");
+    exit(-1);
+  }
 
-    fd = open(argv[1], O_RDWR | O_NOCTTY );
-    if (fd <0) {perror(argv[1]); exit(-1); }
+  bzero(&newtio, sizeof(newtio));
+  newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+  newtio.c_iflag = IGNPAR;
+  newtio.c_oflag = 0;
 
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
+  /* set input mode (non-canonical, no echo,...) */
+  newtio.c_lflag = 0;
 
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
+  newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+  newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
 
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
+/*
+  VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+  leitura do(s) pr�ximo(s) caracter(es)
+*/
 
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
+  tcflush(fd, TCIOFLUSH);
 
+  if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
+  printf("New termios structure set\n");
 
-  /*
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) pr�ximo(s) caracter(es)
-  */
+  printf("Diga algo: ");
+  gets(buf);
+  buf[4]='\0';
 
+  res = write(fd,buf,255);
+  printf("%d bytes written\n", res);
 
+/*
+  O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar
+  o indicado no gui�o
+*/
 
-    tcflush(fd, TCIOFLUSH);
+  if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+    perror("tcsetattr");
+    exit(-1);
+  }
 
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-    printf("New termios structure set\n");
-
-
-
-    for (i = 0; i < 255; i++) {
-      buf[i] = 'a';
-    }
-
-    /*testing*/
-    buf[25] = '\n';
-
-    res = write(fd,buf,255);
-    printf("%d bytes written\n", res);
-
-
-  /*
-    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar
-    o indicado no gui�o
-  */
-
-
-
-
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-
-
-
-    close(fd);
-    return 0;
+  close(fd);
+  return 0;
 }
