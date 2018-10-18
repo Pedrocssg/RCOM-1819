fc344b118e1e23a449a65897bd4d7a829ea682c0
claudiolemos <cflemos1999@me.com>
2018-10-18 19:28:56 +0100

    Write with timeout

--- macros.h
+++ macros.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
 
 #define BAUDRATE B9600
 #define MODEMDEVICE "/dev/ttyS1"
--- writenoncanonical.c
+++ writenoncanonical.c
@@ -3,11 +3,19 @@
 unsigned char set[5] = {FLAG,A,SET_C,SET_BCC,FLAG};
 unsigned char ua[5] = {FLAG,A,UA_C,UA_BCC,FLAG};
 volatile int STOP=FALSE;
+int flag=1, conta=1;
+
+void atende()                   // atende alarme
+{
+  printf("alarme # %d\n", conta);
+  flag=1;
+  conta++;
+}
 
 int main(int argc, char const *argv[]) {
   int fd,c, res;
   struct termios oldtio,newtio;
-  char buf[255];
+  unsigned char buf[255];
   int i, sum = 0, speed = 0;
 
   if ( (argc < 2) ||
@@ -16,6 +24,8 @@ int main(int argc, char const *argv[]) {
     printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
     exit(1);
   }
+
+  (void) signal(SIGALRM, atende);  // instala  rotina que atende interrupcao
 
 /*
   Open serial port device for reading and writing and not as controlling tty
@@ -38,7 +48,7 @@ int main(int argc, char const *argv[]) {
   /* set input mode (non-canonical, no echo,...) */
   newtio.c_lflag = 0;
 
-  newtio.c_cc[VTIME]    = 30;   /* inter-character timer unused */
+  newtio.c_cc[VTIME]    = 0;   /* inter-character used */
   newtio.c_cc[VMIN]     = 0;   /* blocking read until n chars received */
 
 /*
@@ -55,56 +65,57 @@ int main(int argc, char const *argv[]) {
 
   printf("New termios structure set\n");
 
-  if ((res = write(fd,set,5)) == -1) {
-      printf("An error has occured.\n");
-      exit(-1);
-  }
+  i=0;
+  while(conta < 4 && STOP == FALSE){
+      if(flag){
+          alarm(3);                 // activa alarme de 3s
+          flag=0;
 
-  printf("%d bytes written\n", res);
+          if ((res = write(fd,set,5)) == -1) {
+              printf("An error has occured writing the message.\n");
+              exit(-1);
+          }
 
-/*
-  O ciclo FOR e as instrucoes seguintes devem ser alterados de modo a respeitar
-  o indicado no guiao
-*/
-  i = 0;
-  while (STOP == FALSE) {
+          printf("%d bytes written\n", res);
+      }
+
       res = read(fd, &buf[i], 1);
 
+      
       if (res < 0) {
-          printf("You are dumb af...\n");
+          printf("There was an error while reading the buffer.\n");
           exit(-1);
+      }
+      else if (res == 0) {
+          continue;
       }
       else {
           switch (i) {
             case 0:
-                if (buf[i] == set[i])
+                if (buf[i] == ua[i])
                     i++;
-                else
-                    i--;
                 break;
             case 1:
-                if (buf[i] == set[i])
+                if (buf[i] == ua[i])
                     i++;
                 else
                     i--;
                 break;
             case 2:
-                if (buf[i] == set[i])
+                if (buf[i] == ua[i])
                     i++;
                 else
                     i--;
                 break;
             case 3:
-                if (buf[i] == set[i])
+                if (buf[i] == ua[i])
                     i++;
                 else
                     i--;
                 break;
             case 4:
-                if (buf[i] == set[i]) {
-                    i++;
+                if (buf[i] == ua[i])
                     STOP = TRUE;
-                }
                 else
                     i--;
                 break;
@@ -115,8 +126,6 @@ int main(int argc, char const *argv[]) {
           }
       }
   }
-
-  printf("%s:%d\n", buf, i);
 
   if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
     perror("tcsetattr");
