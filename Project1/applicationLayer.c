2270c02fbf9730faf721b6198536a677f4b58736
claudiolemos <cflemos1999@me.com>
2018-11-04 17:08:10 +0000

    Added clock

--- Project1/applicationLayer.c
+++ Project1/applicationLayer.c
@@ -57,7 +57,6 @@ int main(int argc, char const *argv[]) {
 
 
     gettimeofday(&stop, NULL);
-    
     secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 + (double)(stop.tv_sec - start.tv_sec);
     printf("time taken %f\n",secs);
 
