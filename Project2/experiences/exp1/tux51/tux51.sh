e144d70dd725e71312c37b0e73d176a492a21409
Duarte Oliveira <duartefmoliveira8@gmail.com>
2018-12-18 14:22:09 +0000

    exp1 script

--- Project2/experiences/exp1/tux51/tux51.sh
+++ Project2/experiences/exp1/tux51/tux51.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
+/etc/init.d/networking restart
 ifconfig eth0 up
 ifconfig eth0 172.16.50.1/24
 route add default gw 172.16.50.254
-echo 1 > /proc/sys/net/ipv4/ip_forward
-echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
+#connect cables
