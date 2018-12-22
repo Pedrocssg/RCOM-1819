5023638950118e1b4a29b55905866108e2adb1c3
Pedro Gonçalves <pedrokraktuga@gmail.com>
2018-12-22 12:13:54 +0000

    Update tux54.sh

--- Project2/experiences/exp3/tux54/tux54.sh
+++ Project2/experiences/exp3/tux54/tux54.sh
@@ -9,7 +9,7 @@ echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
 route del default gw 172.16.50.1
 
 # configure terminal
-# interface fastethernet 0/4
+# interface fastethernet 0/7
 # switchport mode access
 # switchport access vlan 51
 # end
